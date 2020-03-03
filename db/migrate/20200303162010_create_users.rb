ef40845508cf88e6de6b5d9b174d9b132e9d1090
vanvietbao <vanvietbao0980@gmail.com>
2020-03-03 11:06:12 +0700

    init

---
+++ db/migrate/20200303162010_create_users.rb
@@ -0,0 +1,19 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :name
+      t.text :skill
+      t.date :birthday
+      t.string :phone
+      t.integer :staff_type, default: 0
+      t.integer :workspace, default: 0
+      t.integer :gender, default: 0
+      t.integer :nationality, default: 0
+      t.integer :position, default: 0
+      t.integer :role, default: 0
+
+      t.timestamps
+    end
+  end
+end
