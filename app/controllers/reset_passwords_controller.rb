ac3aaa4ce22c32766a96e7ff41519578c5325251
hung <hungtranpro2@gmail.com>
2020-03-26 08:35:10 +0700

    reset_password

---
+++ app/controllers/reset_passwords_controller.rb
@@ -0,0 +1,31 @@
+class ResetPasswordsController < ApplicationController
+    def edit
+    @user = User.find_by id: params[:id]
+  end
+
+  def update
+    @user = User.find_by id: params[:id]
+    if params[:user][:password].empty?
+      @user.errors.add(:password,"can't be empty")
+      flash.now[:error] = "Password can't empty"
+      render :edit
+    elsif params[:user][:password] != params[:user][:password_confirmation]
+      @user.errors.add(:password, "false confirmation")
+      flash.now[:error] = "False confirmation"
+      render :edit
+    elsif @user.update_attributes(user_params)
+      sign_in @user
+      flash[:success] = "Password has been reset."
+      redirect_to profile_path(@user)
+    else
+      flash.now[:error] = "Password reset failed"
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit User::PARAMS_PASSWORD
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
 	resources :static_pages
   resources :room_chats
   resources :messages
-  resources :profiles, only: [:show, :edit, :update]
+  resources :profiles, only: [:edit, :update]
   resources :message_notifications, only: [:index]
+  resources :reset_passwords, only: [:edit ,:update]
   devise_for :users
   # get 'static_pages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
