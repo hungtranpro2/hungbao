45d11318a246121a4bf8a19481667b874ef17939
vanvietbao <vanvietbao0980@gmail.com>
2020-04-05 16:53:05 +0700

    Update Approval Request

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
   resources :show_profiles
   resources :project_divisions
   resources :show_reports
+  resources :approval_requests
   devise_for :users
   # get 'static_pages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
