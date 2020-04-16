67c8958864bf032906a998664d26a319a8db051d
vanvietbao <vanvietbao0980@gmail.com>
2020-04-16 10:28:35 +0700

    Up chat

--- app/controllers/add_project_divisions_controller.rb
+++ app/controllers/add_project_divisions_controller.rb
@@ -13,12 +13,22 @@ class AddProjectDivisionsController < ApplicationController
   def create
     @project = Project.find_by id: params[:project_id]
     @user_project = UserProject.new user_id: params[:user_id], project_id: params[:project_id]
-    if @user_project.save
+    ActiveRecord::Base.transaction do
+      @user_project.save!
+      add_user_to_chat params[:user_id], @project
       flash[:success] = "Thêm quản lý bộ phận thành công"
       redirect_to company_projects_path
-    else
+    rescue ActiveRecord::RecordInvalid
       flash[:error] = "Thêm quản lý bộ phận thất bại"
       redirect_to root_path
     end
+  end
+
+  private
+
+  def add_user_to_chat user_id, project
+    user = User.find_by id: user_id
+    list = ListMessage.find_by name: project.name
+    Message.create!(content: "#{user.name} đã tham gia dự án", list_message_id: list.id, user_id: user_id)
   end
 end
