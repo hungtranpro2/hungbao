5c1bdefd536e9e22f4d87160b8cf3ba0fed43d4d
vanvietbao <vanvietbao0980@gmail.com>
2020-04-17 12:45:33 +0700

    fix

--- app/controllers/overview_companys_controller.rb
+++ app/controllers/overview_companys_controller.rb
@@ -1,23 +1,50 @@
 class OverviewCompanysController < ApplicationController
   before_action :authenticate_user!
   before_action :count_overview, only: [:index]
+  before_action :time_line, only: [:index]
+  before_action :pie_chart_project, only: [:index]
+  before_action :correct_boss
+
   def index
-    @tasks = Task.where(parent_task: true, active: true)
-    time_line @tasks
-    @children_tasks = current_division.tasks.where(parent_task: false, active: true).where.not(project_id: nil)
+    @q = Project.ransack(params[:q])
+    @projects = @q.result(distinct: true).paginate(page: params[:page], per_page: 7)
   end
 
   private
-  def time_line tasks
+
+  def time_line
     @time_line = []
-    tasks.each do |task|
-      @time_line.push(["#{task.title}", "#{task.start_time}", "#{task.end_time + 1.days}"])
+    @projects = Project.all
+    @projects.each do |project|
+      start_time = project.tasks.where(parent_task: true).minimum('start_time')
+      end_time = project.tasks.where(parent_task: true).maximum('end_time')
+      if start_time.present? && end_time.present?
+        @time_line.push(["#{project.name}", "#{start_time}", "#{end_time + 1.days}"])
+      end
     end
+  end
+
+  def pie_chart_project
+    @pie_chart_project = []
+    @projects = Project.all
+    @projects.each do |project|
+      pie_chart_division = []
+      project.divisions.uniq.each do |division|
+        pie_chart_division.push(["#{division.name}", "#{division.users.where(id: project.users).count}"])
+      end
+      @pie_chart_project.push(pie_chart_division)
+    end
+    @sum_pie = @pie_chart_project.count - 1
   end
 
   def count_overview
     @project_count = Project.all.count
     @report_count = Report.all.count
-    @task_count = Task.where(parent_task: true).count
+  end
+
+  def correct_boss
+    unless current_user.manager? && current_division.is_project?
+      redirect_to root_path
+    end
   end
 end
--- app/controllers/project_tasks_controller.rb
+++ app/controllers/project_tasks_controller.rb
@@ -1,8 +1,15 @@
 class ProjectTasksController < ApplicationController
    before_action :authenticate_user!
-
+   before_action :correct_project_leader
   def index
-    @q = current_user.projects.first.tasks.where.not(project_id: nil).ransack(params[:q])
+    @q = current_user.projects.first.tasks.where.not(project_id: nil).where(parent_task: true).ransack(params[:q])
     @tasks = @q.result.paginate(page: params[:page], per_page: 10)
+  end
+
+  private
+  def correct_project_leader
+    unless current_user.member? && current_division.is_project?
+      redirect_to root_path
+    end
   end
 end
